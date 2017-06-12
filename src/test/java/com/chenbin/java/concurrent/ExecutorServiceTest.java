42d64579a760ea2ee2cd45d37255997c5200fd9f
chenbin1314 <c1325791024@163.com>
2017-06-12 22:57:31 +0800

    Init commit.

---
+++ src/test/java/com/chenbin/java/concurrent/ExecutorServiceTest.java
@@ -0,0 +1,15 @@
+package com.chenbin.java.concurrent;
+
+import org.junit.Test;
+
+/**
+ * Created by chenbin on 2017/6/12.
+ */
+public class ExecutorServiceTest {
+
+    @Test
+    public void test1() {
+        System.out.println("Executor service");
+    }
+
+}
