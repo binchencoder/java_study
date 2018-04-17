d5a19c2064cc487796a11ab86a602b7b7af6635a
binchen <c1325791024@163.com>
2018-04-17 14:32:52 +0800

    Add ThreadTest case.

---
+++ src/test/java/com/chenbin/study/displacement/TestUnit.java
@@ -0,0 +1,14 @@
+package com.chenbin.study.displacement;
+
+import org.junit.Test;
+
+/**
+ * Created by chenbin on 18-4-16.
+ */
+public class TestUnit {
+
+  @Test
+  public void test1() {
+    System.out.println(16 >> 1);
+  }
+}
