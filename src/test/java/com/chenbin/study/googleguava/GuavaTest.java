054e63e46c203d1cc724d46bc6ae175b5db47570
binchen <c1325791024@163.com>
2018-05-03 15:45:08 +0800

    Add google guava test cases.

---
+++ src/test/java/com/chenbin/study/googleguava/GuavaTest.java
@@ -0,0 +1,38 @@
+package com.chenbin.study.googleguava;
+
+import com.google.common.collect.Lists;
+import com.google.common.math.IntMath;
+import java.math.RoundingMode;
+import java.util.List;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by chenbin on 18-5-3.
+ */
+public class GuavaTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(GuavaTest.class);
+
+  public static List<String> lst = Lists.newArrayList();
+
+  @BeforeClass
+  public static void init() {
+    for (int i = 0; i < 100; i++) {
+      lst.add("guava." + i);
+    }
+
+    lst.add(null);
+  }
+
+  @Test
+  public void partitionTest() {
+    int partitionSize = IntMath.divide(lst.size(), 30, RoundingMode.DOWN);
+    List<List<String>> partitions = Lists.partition(lst, partitionSize);
+
+    logger.debug("partitionSize: {}, partitions:{}", partitionSize, partitions);
+  }
+
+}
