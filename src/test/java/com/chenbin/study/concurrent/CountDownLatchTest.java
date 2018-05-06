bf1612991103fe0f68fd20472cb04c1d5d11ab14
chenbin1314 <c1325791024@163.com>
2018-05-06 17:27:21 +0800

    CountDownLatchTest 增加对异常的处理

--- src/test/java/com/chenbin/study/concurrent/CountDownLatchTest.java
+++ src/test/java/com/chenbin/study/concurrent/CountDownLatchTest.java
@@ -1,8 +1,10 @@
 package com.chenbin.study.concurrent;
 
 import com.google.common.collect.Lists;
+import java.io.InvalidClassException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.CountDownLatch;
 import org.junit.Test;
 
@@ -48,35 +50,91 @@ public class CountDownLatchTest {
   }
 
   @Test
-  public void testAsyncExecSuccedOfCountDown() {
+  public void testAsyncExecSuccedOfCountDown() throws Exception {
     List<String> options = Lists.newArrayList("1", "2", "3");
     long start = System.currentTimeMillis();
 
     CountDownLatch latch = new CountDownLatch(options.size());
 
-    options.stream().forEach(op -> CompletableFuture.runAsync(() -> {
-      try {
-        switch (op) {
-          case "1":
-            Thread.sleep(1000L);
-            System.out.println("Get 1");
-            break;
-          case "2":
-            Thread.sleep(2000L);
-            System.out.println("Get 2");
-            break;
-          case "3":
-            Thread.sleep(3000L);
-            System.out.println("Get 3");
-            break;
-          default:
-            break;
+    CompletableFuture completableFuture = new CompletableFuture<>();
+    options.stream().forEach(op -> {
+      completableFuture.supplyAsync(() -> {
+        try {
+          switch (op) {
+            case "1":
+              Thread.sleep(1000L);
+              System.out.println("Get 1");
+              break;
+            case "2":
+              throwException();
+              Thread.sleep(2000L);
+              System.out.println("Get 2");
+              break;
+            case "3":
+              Thread.sleep(3000L);
+              System.out.println("Get 3");
+              break;
+            default:
+              break;
+          }
+        } catch (Exception ex) {
+          System.err.println("Occur exception");
+          completableFuture.completeExceptionally(ex);
+        } finally {
+          latch.countDown();
         }
-      } catch (Exception ex) {
-      } finally {
-        latch.countDown();
+        return null;
+      });
+    });
+
+    boolean exceptionally = completableFuture.exceptionally(ex -> {
+      System.err.println("exceptionally");
+      throw new CompletionException("CompleteFuture exceptionally", (Throwable) ex);
+    }).isCompletedExceptionally();
+
+    System.err.println("CompletableFuture exceptionally is: " + exceptionally);
+    if (exceptionally) {
+      throw new RuntimeException("");
+    }
+
+    /*options.stream().forEach(op -> {
+      CompletableFuture completableFuture = CompletableFuture.runAsync(() -> {
+        try {
+          switch (op) {
+            case "1":
+              Thread.sleep(1000L);
+              System.out.println("Get 1");
+              break;
+            case "2":
+              throwException();
+              Thread.sleep(2000L);
+              System.out.println("Get 2");
+              break;
+            case "3":
+              Thread.sleep(3000L);
+              System.out.println("Get 3");
+              break;
+            default:
+              break;
+          }
+        } catch (Exception ex) {
+          System.err.println("Occur exception");
+          throw new RuntimeException();
+        } finally {
+          latch.countDown();
+        }
+      });
+
+      boolean exceptionally = completableFuture.exceptionally(ex -> {
+        System.err.println("exceptionally");
+        throw new RuntimeException();
+      }).isCompletedExceptionally();
+
+      System.err.println(exceptionally);
+      if (exceptionally) {
+        throw new RuntimeException();
       }
-    }));
+    });*/
 
     try {
       latch.await();
@@ -86,5 +144,9 @@ public class CountDownLatchTest {
 
     System.out.println("Cost time:" + (System.currentTimeMillis() - start));
     System.out.println("Over");
+  }
+
+  private void throwException() {
+    throw new RuntimeException("Occur exception");
   }
 }
