1a4cd409223b4dfe60ea8050e2245e5c3564feea
binchen <c1325791024@163.com>
2019-08-20 18:36:30 +0800

    实现生产者消费者模式

---
+++ src/main/java/com/binchencoder/study/wait/Main.java
@@ -0,0 +1,28 @@
+package com.binchencoder.study.wait;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Object lock = new Object();
+    Storage storage = new Storage();
+
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e1) {
+      e1.printStackTrace();
+    }
+
+    new Thread(new Customer(storage, lock)).start();
+    new Thread(new Producer(storage, lock)).start();
+    new Thread(new Customer(storage, lock)).start();
+    new Thread(new Producer(storage, lock)).start();
+    new Thread(new Customer(storage, lock)).start();
+    new Thread(new Producer(storage, lock)).start();
+    new Thread(new Customer(storage, lock)).start();
+    new Thread(new Producer(storage, lock)).start();
+    new Thread(new Customer(storage, lock)).start();
+    new Thread(new Producer(storage, lock)).start();
+    new Thread(new Customer(storage, lock)).start();
+    new Thread(new Producer(storage, lock)).start();
+  }
+}
